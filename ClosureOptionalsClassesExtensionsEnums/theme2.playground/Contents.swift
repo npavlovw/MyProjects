efae8dd74963a3a569439f3b2496d4522534d2b6
Никита Павловя <nekit98nikitapavlov@yandex.rum>
2025-03-16 21:14:55 +0300

    Выполнил второе задание (Замыкания)

--- ClosureOptionalsClassesExtensionsEnums/theme2.playground/Contents.swift
+++ ClosureOptionalsClassesExtensionsEnums/theme2.playground/Contents.swift
@@ -30,3 +30,30 @@ func numberSquared2 (number: Int?) -> Int? {
 
 numberSquared2(number: 7)
 numberSquared2(number: nil)
+
+
+
+//Замыкания
+/* Создайте функцию, которая принимает массив чисел ( [Int]) и замыкание, которое проверяет, удовлетворяет ли элемент заданному условию (тип замыкания: (Int) -> Bool ). Функция должна вернуть новый массив, состоящий только из тех элементов, для которых замыкание возвращает true.
+ 
+ Реализуйте логику фильтрации внутри функции, не используя стандартный метод filter
+ Продемонстрируйте работу функции, передав в качестве замыкания условие для выбора чётных чисел.*/
+
+func filter (array: [Int], condition: ((Int) -> Bool)?) -> [Int] {
+    guard let condition else {
+        print ("Условия не задано")
+        return []
+    }
+    var filterdArray: [Int] = []
+    for i in array {
+        if condition(i) {
+            filterdArray.append(i)
+        }
+    }
+    print (filterdArray)
+    return filterdArray
+}
+
+filter(array: [1, 2, 3, 4, 5, 6]) { i in
+    i % 2 == 0
+}
