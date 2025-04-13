92d3ec59f36450d43ca471a79784fbf6f83f7acd
Никита Павловя <nekit98nikitapavlov@yandex.rum>
2025-04-14 00:58:27 +0300

    Добавил таймер на VerificationViewController

--- FoodDeliveryApp/FoodDeliveryApp/ViewControllers/VerificationViewController.swift
+++ FoodDeliveryApp/FoodDeliveryApp/ViewControllers/VerificationViewController.swift
@@ -23,17 +23,48 @@ class VerificationViewController: UIViewController {
         return label
     }()
     lazy var contentView = customContentView()
+    lazy var codeLabel: UILabel = {
+        let codeLabel = UILabel()
+        codeLabel.text = "CODE"
+        codeLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        codeLabel.textColor = .black
+        return codeLabel
+    }()
+    private var timer: Timer?
+    private var secondsLeft = 60
+    lazy var resendCodeButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.isEnabled = false
+        button.setTitle("Reset", for: .normal)
+        button.setTitleColor(.gray, for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        return button
+    }()
+    lazy var resendLabel: UILabel = {
+        let label = UILabel()
+        label.text = "in 60 sec"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        return label
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .logInBackground
         
+        setupUI()
+    }
+    
+    private func setupUI() {
         setupImages()
         setupBackButton()
         setupLabels()
         setupEmailCode()
         setupContentView()
-
+        setupCodeLabel()
+        setupResendCode()
+        startTimer()
     }
    
     private func setupImages() {
@@ -67,7 +98,7 @@ class VerificationViewController: UIViewController {
         view.addSubview(emailCode)
         
         emailCode.snp.makeConstraints { make in
-            make.top.equalTo(mainLabels.snp.bottom).inset(4)
+            make.top.equalTo(mainLabels.snp.bottom).offset(6)
             make.centerX.equalToSuperview()
         }
     }
@@ -82,10 +113,88 @@ class VerificationViewController: UIViewController {
         }
     }
     
+    private func setupCodeLabel() {
+        contentView.addSubview(codeLabel)
+        
+        codeLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().inset(24)
+        }
+    }
+
+    private func setupResendCode() {
+        contentView.addSubview(resendLabel)
+        contentView.addSubview(resendCodeButton)
+        
+        resendLabel.snp.makeConstraints { make in
+            make.centerY.equalTo(codeLabel.snp.centerY)
+            make.trailing.equalToSuperview().offset(-24)
+        }
+        resendCodeButton.snp.makeConstraints { make in
+            make.centerY.equalTo(resendLabel.snp.centerY)
+            make.trailing.equalTo(resendLabel.snp.leading).offset(-2)
+        }
+        
+        resendCodeButton.addTarget(self, action: #selector(resendTapped), for: .touchUpInside)
+    }
+    
+    private func startTimer() {
+        timer?.invalidate()
+        secondsLeft = 60
+        resendLabel.text = "in \(secondsLeft) sec"
+        
+        UIView.performWithoutAnimation {
+            let attributedTitle = NSAttributedString(
+                string: "Resend",
+                attributes: [
+                    .font: UIFont.systemFont(ofSize: 14, weight: .regular),
+                    .underlineStyle: 0
+                ]
+            )
+            resendCodeButton.setAttributedTitle(attributedTitle, for: .normal)
+            resendCodeButton.layoutIfNeeded()
+        }
+        resendCodeButton.isEnabled = false
+        resendCodeButton.setTitleColor(.gray, for: .normal)
+        updateButtonTitle()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    private func updateButtonTitle() {
+        resendLabel.text = "in \(secondsLeft) sec"
+    }
+    
+    @objc private func updateTimer() {
+        secondsLeft -= 1
+        updateButtonTitle()
+        
+        if secondsLeft <= 0 {
+            resendLabel.text = ""
+            timer?.invalidate()
+            UIView.performWithoutAnimation {
+                let attributedTitle = NSAttributedString(
+                    string: "Resend",
+                    attributes: [
+                        .font: UIFont.systemFont(ofSize: 14, weight: .bold),
+                        .underlineStyle: NSUnderlineStyle.single.rawValue
+                    ]
+                )
+                resendCodeButton.setAttributedTitle(attributedTitle, for: .normal)
+                resendCodeButton.layoutIfNeeded()
+            }
+            resendCodeButton.isEnabled = true
+            resendCodeButton.setTitleColor(.black, for: .normal)
+        }
+    }
+    
     @objc private func backToLForgotPassword() {
         let forgotPasswordVC = ForgotPasswordViewController()
         forgotPasswordVC.modalPresentationStyle = .fullScreen
         present(forgotPasswordVC, animated: false)
     }
     
+    @objc private func resendTapped() {
+        print("Таймер запущен заново")
+        startTimer()
+    }
 }
